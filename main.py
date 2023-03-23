4579e0e603bb2b03899e643148b296dba04857f9
Seulgigi <2021112414@dgu.ac.kr>
2023-03-23 13:46:14 +0900

    2023-03-23 실습과제#4 4번

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 name = input('이름을 입력하세요: ')
 number = int(input('학번을 입력하세요: '))
+major = input('학과를 입력하세요: ')
 
-print(f'\n<출력>\n이름: {name}\n학번: {number}')
+print(f'\n<출력>\n이름: {name}\n학번: {number}\n학과: {major}')
